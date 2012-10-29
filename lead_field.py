75b17fb8973315d8d19f619bca0b384d18a8e224
Roman Goj <roman.goj@gmail.com>
2012-10-29 11:48:27 +0000

    Moving calculation of dipole coordinates out of electrode loop so that it is not needlessly repeated

--- lead_field.py
+++ lead_field.py
@@ -8,7 +8,7 @@ sys.path.insert(0, 'old/scalingproject')
 sys.path.insert(0, 'old/src')
 from topographicmap import read_electrode_locations
 
-@profile
+#@profile
 def calculate_lead_field(gen_conf):
     # Reading in electrode locations from external file electrodeLocations.elp
     [el, el_x, el_y, el_thetas, el_phis] = read_electrode_locations()
@@ -25,7 +25,40 @@ def calculate_lead_field(gen_conf):
 
     # The number of electrodes and generators defines the size of the lead field matrix
     lead_field_brody_1973 = zeros((n_el,n_gen))
-      
+
+    # Coordinates of each dipole
+    xyz_orientation_dipole = []
+    for i_gen in range(n_gen):
+        # The Orientation vector
+        orientation_theta = gen_conf[i_gen]['orientation']
+        orientation_phi = gen_conf[i_gen]['orientation_phi']
+        xyz_orientation_dipole_rotated = zeros(3);
+        xyz_orientation_dipole_rotated[0] = sin(orientation_theta) * cos(orientation_phi);
+        xyz_orientation_dipole_rotated[1] = sin(orientation_theta) * sin(orientation_phi);
+        xyz_orientation_dipole_rotated[2] = cos(orientation_theta);
+        
+        # Rotation matrix for translating the coordinates in the dipole frame of reference to the
+        # coordinates associated with the dipole parallel to the head coordinates.
+        rotation_matrix = zeros((3,3));
+        dipole_theta = gen_conf[i_gen]['theta']
+        dipole_phi = gen_conf[i_gen]['phi']
+        # Row 1
+        rotation_matrix[0,0] = sin(dipole_phi)
+        rotation_matrix[0,1] = cos(dipole_theta) * cos(dipole_phi)
+        rotation_matrix[0,2] = sin(dipole_theta) * cos(dipole_phi)
+        # Row 2
+        rotation_matrix[1,0] = -cos(dipole_phi)
+        rotation_matrix[1,1] = cos(dipole_theta) * sin(dipole_phi)
+        rotation_matrix[1,2] = sin(dipole_theta) * sin(dipole_phi)
+        # Row 3
+        rotation_matrix[2,0] = 0
+        rotation_matrix[2,1] = -sin(dipole_theta)
+        rotation_matrix[2,2] = cos(dipole_theta)
+            
+        # Rotating Orientation to translated dipole coordinates
+        xyz_orientation_dipole.append(dot(rotation_matrix,xyz_orientation_dipole_rotated))
+
+
     for i_el in range(n_el):
         # Calculating the coordinates of the electrode in the Cartesian coordinates associated with the head
         # The X axis points towards the right ear, while the Y axis points towards the front
@@ -58,14 +91,6 @@ def calculate_lead_field(gen_conf):
             distance += pow(xyz_el_dipole[1],2.0)
             distance += pow(xyz_el_dipole[2],2.0)
             distance = sqrt(distance)
-
-            # The Orientation vector
-            orientation_theta = gen_conf[i_gen]['orientation']
-            orientation_phi = gen_conf[i_gen]['orientation_phi']
-            xyz_orientation_dipole_rotated = zeros(3);
-            xyz_orientation_dipole_rotated[0] = sin(orientation_theta) * cos(orientation_phi);
-            xyz_orientation_dipole_rotated[1] = sin(orientation_theta) * sin(orientation_phi);
-            xyz_orientation_dipole_rotated[2] = cos(orientation_theta);
             
             #
             # Bounded spherical conductor
@@ -79,27 +104,6 @@ def calculate_lead_field(gen_conf):
                 field_vector[i] += (1/pow(radius,2.0)) * (xyz_el_head[i] + (xyz_el_head[i] * r_cos_phi - radius * xyz_dipole_head[i])/(distance + radius - r_cos_phi))
                 field_vector[i] = field_vector[i] / 4 / pi / sigma / distance
             
-            # Rotation matrix for translating the coordinates in the dipole frame of reference to the
-            # coordinates associated with the dipole parallel to the head coordinates.
-            rotation_matrix = zeros((3,3));
-            dipole_theta = gen_conf[i_gen]['theta']
-            dipole_phi = gen_conf[i_gen]['phi']
-            # Row 1
-            rotation_matrix[0,0] = sin(dipole_phi)
-            rotation_matrix[0,1] = cos(dipole_theta) * cos(dipole_phi)
-            rotation_matrix[0,2] = sin(dipole_theta) * cos(dipole_phi)
-            # Row 2
-            rotation_matrix[1,0] = -cos(dipole_phi)
-            rotation_matrix[1,1] = cos(dipole_theta) * sin(dipole_phi)
-            rotation_matrix[1,2] = sin(dipole_theta) * sin(dipole_phi)
-            # Row 3
-            rotation_matrix[2,0] = 0
-            rotation_matrix[2,1] = -sin(dipole_theta)
-            rotation_matrix[2,2] = cos(dipole_theta)
-                
-            # Rotating Orientation to translated dipole coordinates
-            xyz_orientation_dipole = dot(rotation_matrix, xyz_orientation_dipole_rotated)
-                
-            lead_field_brody_1973[i_el, i_gen] = dot(field_vector, xyz_orientation_dipole)
+            lead_field_brody_1973[i_el, i_gen] = dot(field_vector,xyz_orientation_dipole[i_gen])
     
     return lead_field_brody_1973
