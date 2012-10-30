0f1d01e897d44ebfac9da3068bc877700fad30f6
Roman Goj <roman.goj@gmail.com>
2012-10-30 13:05:27 +0000

    Need to assert arrays as *almost* equal, not just equal

--- test_lead_field.py
+++ test_lead_field.py
@@ -2,6 +2,7 @@ import pickle
 
 from nose import with_setup
 from numpy import array_equal
+from numpy.testing import assert_array_almost_equal
 
 from lead_field import calculate_lead_field
 
@@ -18,4 +19,4 @@ def test_lead_field_same_output_as_old_code():
     global old_code
     for i in range(len(old_code)):
         lead_field = calculate_lead_field(old_code[i]['gen_conf'])
-        assert array_equal(lead_field, old_code[i]['lead_field'])
+        assert_array_almost_equal(lead_field, old_code[i]['lead_field'], 17)
